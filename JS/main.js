45f36980c538b457c90e33ea39cce9c5b2488b9f
JustDarkAmigo <JustDarkAmigo>
2020-12-20 15:43:16 +0800

    .

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,51 @@
-let Flag_menu = false;
 
-document.getElementById("abc").onclick = function(){
-    if (Flag_menu){
-        document.getElementById("books_SB").style.display = "none";
-    Flag_menu = false;
-    document.getElementById("header").style.background = "green";
+let list_book = document.getElementsByClassName("book");
+document.getElementById("show_books").onclick = function(){
+    document.getElementById("show_books").style.display = "none";
+    for (let i = 0; i < list_book.length; i++){
+        list_book[i].classList.remove('hidden_book');
     }
-    else{
-        document.getElementById("books_SB").style.display = "block";
-        Flag_menu = true;
-        document.getElementById("header").style.background = "tomato";
-    }
+
 };
+
+var toggle = document.querySelector('[name="Author"]');
+
+toggle.addEventListener('change', function() { 
+    var chosenClass = this[this.selectedIndex].value;
+    console.log(chosenClass)
+    
+	var items = document.querySelectorAll('.book');
+    console.log(items)
+    Array.prototype.forEach.call(items, function(item) {
+        var child = item.children[0];
+        var childClasses = child.className.split(' ');
+        console.log(childClasses, child)
+        if (childClasses.indexOf(chosenClass) === -1) {
+            item.style.display='none';
+        } else {
+            item.style.display='block';
+        }
+    });
+});
+var toggle = document.querySelector('[name="subject"]');
+
+toggle.addEventListener('change', function() { 
+    var chosenClass = this[this.selectedIndex].value;
+    console.log(chosenClass)
+    
+	var items = document.querySelectorAll('.book');
+    console.log(items)
+    Array.prototype.forEach.call(items, function(item) {
+        var child = item.children[0];
+        var childClasses = child.className.split(' ');
+        console.log(childClasses, child)
+        if (childClasses.indexOf(chosenClass) === -1) {
+            item.style.display='none';
+        } else {
+            item.style.display='block';
+        }
+        
+    });
+});
+
+
